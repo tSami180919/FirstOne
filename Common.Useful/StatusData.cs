8922a7ab4616671eb6be3966b7816c5ae64d9046
My Name <me@example.com>
2019-09-18 22:53:49 +0200

    Creation of Common.Useful

---
+++ Common.Useful/StatusData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Common.Useful
+{
+    public struct StatusData<T> 
+    {
+        private readonly T value;
+        private readonly string errorMessage;
+        public readonly bool IsError;
+
+        public T Value
+        {
+            get {
+                if (IsError)
+                    throw new Exception($"Cannot Acces Value, Status Data is in Error : {errorMessage}");
+                return value; }
+        }
+
+        public string ErrorMessage
+        {
+            get
+            {
+                if (!IsError)
+                    throw new Exception($"Cannot Acces Error, Status Data is Valid : {value}");
+                return errorMessage;
+            }
+        }
+
+        internal StatusData(string errorMessage, T val, bool isError)
+        {
+            this.errorMessage = errorMessage;
+            this.value = val;
+            this.IsError = isError;
+        }
+    }
+}
---
+++ Common.Useful/StatusDataBuilder.cs
@@ -0,0 +1,20 @@
+﻿namespace Common.Useful
+{
+    public static class StatusDataBuilder
+    {
+
+        public static StatusData<B> Create<B>(string errorMessage)
+        {
+            return new StatusData<B>(errorMessage, default, true);
+        }
+
+        public static StatusData<B> Create<B>(B val)
+        {
+            return new StatusData<B>(string.Empty, val, false);
+        }
+
+
+    }
+
+
+}
